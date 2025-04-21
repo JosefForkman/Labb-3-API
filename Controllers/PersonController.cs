0cd2a0202690410cbfc681718f1294a5e42ed040
JosefForkman <44498331+JosefForkman@users.noreply.github.com>
2025-04-21 18:10:43 +0100

    Get all persons

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -1,11 +1,29 @@
+using System.Threading.Tasks;
+using Labb_3_API.Data;
+using Labb_3_API.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace Labb_3_API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class PersonController : ControllerBase
+    public class PersonController(DBContext context) : ControllerBase
     {
+
+        [HttpGet(Name = "GetAllPersons")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<ICollection<Person>>> Get()
+        {
+            var persons = await context.Persons.ToListAsync();
+            if (persons == null || persons.Count == 0)
+            {
+                return NotFound(new { message = "No persons found." });
+            }
+
+            return Ok(persons);
+        }
     }
 }
