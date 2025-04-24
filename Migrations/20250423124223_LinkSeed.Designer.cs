5f5991e3ee49aa50d722ad577b65ff046c01da6d
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2025-04-24 11:04:52 +0200

    Link Seed Table

--- Migrations/20250423124223_LinkSeed.Designer.cs
+++ Migrations/20250423124223_LinkSeed.Designer.cs
@@ -4,6 +4,7 @@ using Labb_3_API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Labb_3_API.Migrations
 {
     [DbContext(typeof(DBContext))]
-    partial class DBContextModelSnapshot : ModelSnapshot
+    [Migration("20250423124223_LinkSeed")]
+    partial class LinkSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20250423124223_LinkSeed.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Labb_3_API.Migrations
+{
+    /// <inheritdoc />
+    public partial class LinkSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Links",
+                columns: new[] { "Id", "PersonServiceId", "Title", "Url" },
+                values: new object[,]
+                {
+                    { 1, 1, "Länk 1", "https://example.com/link1" },
+                    { 2, 2, "Länk 2", "https://example.com/link2" },
+                    { 3, 3, "Länk 3", "https://example.com/link3" },
+                    { 4, 4, "Länk 4", "https://example.com/link4" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Links",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Links",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Links",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Links",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
